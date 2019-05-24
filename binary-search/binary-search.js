723887ac6ed09d1bdbd4c6b00e71fa75ead712b5
Tal Luigi <luigi.t.agr@gmail.com>
2019-05-24 12:10:24 -0400

    completed binary-search

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,9 +1,23 @@
 'use strict';
 
 // Complete this algo
-const binarySearch = (array, target) => {
-	
-};
+// const binarySearch = (array, target) => {
+//   if (!array.length) {
+//     return false;
+//   }
+//   if (array.length === 1) {
+//     return array[0] === target;
+//   }
+//   const midPoint = Math.floor(array.length / 2);
+//   if (array[midPoint] === target) {
+//     return true;
+//   }
+//   if (array[midPoint] < target) {
+//     return binarySearch(array.slice(midPoint + 1), target);
+//   } else {
+//     return binarySearch(array.slice(0, midPoint), target);
+//   }
+// };
 
 /*
 	EXTRA CREDIT:
@@ -14,4 +28,29 @@ const binarySearch = (array, target) => {
 
 */
 
-module.exports = binarySearch
+const binarySearch = (array, target) => {
+  let curHalf = array;
+  while (curHalf.length) {
+    if (curHalf.length === 1) {
+      return curHalf[0] === target;
+    }
+    const midPoint = Math.floor(curHalf.length / 2);
+    if (curHalf[midPoint] === target) {
+      return true;
+    }
+    let newHalf = [];
+    if (curHalf[midPoint] < target) {
+      for (let i = midPoint + 1; i < curHalf.length; i++) {
+        newHalf.push(curHalf[i]);
+      }
+    } else {
+      for (let i = 0; i < midPoint; i++) {
+        newHalf.push(curHalf[i]);
+      }
+    }
+    curHalf = newHalf;
+  }
+  return false;
+};
+
+module.exports = binarySearch;
