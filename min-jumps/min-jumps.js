9b132b927ce80eb7e2e76d9c3f7da84de98a5d42
Tal Luigi <luigi.t.agr@gmail.com>
2019-05-26 13:08:25 -0400

    completed min-jumps

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,7 +2,29 @@
 
 // Complete this algo
 const minJumps = arr => {
-
+  let steps = 0;
+  let i = 0;
+  while (i < arr.length) {
+    if (arr[i] >= arr.length - 1 - i) {
+      steps++;
+      return steps;
+    } else {
+      const nextStepsIdxArr = [];
+      const nextStepsValuesArr = [];
+      for (let j = i + 1; j <= i + arr[i]; j++) {
+        nextStepsIdxArr.push(j);
+        nextStepsValuesArr.push(arr[j]);
+      }
+      const biggestNextStepValue = Math.max(...nextStepsValuesArr);
+      const biggestNextStepValuesArrIdx = nextStepsValuesArr.lastIndexOf(
+        biggestNextStepValue
+      );
+      const biggestNextStepIdxArrIdx =
+        nextStepsIdxArr[biggestNextStepValuesArrIdx];
+      i = biggestNextStepIdxArrIdx;
+      steps++;
+    }
+  }
 };
 
-module.exports = minJumps
+module.exports = minJumps;
